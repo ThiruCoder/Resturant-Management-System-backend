c925c778dc324106acdceb2ef15c5c758a735fe7
Your Name <ervthirumal14321@gmail.com>
2026-02-10 10:42:17 +0530

    bb15

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000;
 // Middlewares
 app.use(cors({
     origin: function (origin, callback) {
-
+        console.log('Incoming Origin:', origin);
         const allowedOrigins = process.env.FRONTEND_URL ?? 'http://localhost:3000';
 
         // https://resturant-management-system-ruddy.vercel.app
